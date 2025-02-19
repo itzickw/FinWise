88fc75730cfe282ef6df801f45a9365b8c752398
itzickw <80103682+itzickw@users.noreply.github.com>
2025-02-19 17:05:13 +0200

    stock controller

--- StockController/Program.cs
+++ StockController/Program.cs
@@ -1,11 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// הוסף את התלות ב-DbContext וב-StockManager
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<StockManager.StockManager>();
 
 var app = builder.Build();
 
