fac09d09a5844d9da8f06476de5910c97282cb25
itzickw <80103682+itzickw@users.noreply.github.com>
2025-02-20 14:56:50 +0200

    AImodel

---
+++ AIModel/AIResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIModel
+{
+    public class AIResponse
+    {
+        public string Response { get; set; } // טקסט התשובה
+    }
+
+}
